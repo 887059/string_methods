24fd203c77c4e3079d04b8120ad5a7b0f97ca0ae
Parkavi_bhaskaran <157793811+887059@users.noreply.github.com>
2025-02-25 12:12:54 +0530

    uploaded 25-02-2025 12:12 pm

---
+++ ex3.py
@@ -0,0 +1,20 @@
+print("****String Methods****")
+company_name="avary technology"
+deptartment="quality"
+Name= "parkavi\nbhaskaran"
+pin="311"
+print(company_name.capitalize())
+print(company_name.casefold())
+print(company_name.count("tech"))
+print(company_name[0:9])
+print(company_name[-1:-10])
+deptartment=input("enter your department:").upper()
+print(deptartment.upper())
+print(pin.zfill(6))
+print(company_name.title())
+print(Name.strip())
+print(company_name.startswith("avary"))
+print(Name.splitlines())
+
+
+
